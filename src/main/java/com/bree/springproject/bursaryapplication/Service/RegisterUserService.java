8c50edfb220f534547fac341a9fd421e72d5e234
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-01-29 16:01:03 +0300

    Added more layers to the program

---
+++ src/main/java/com/bree/springproject/bursaryapplication/Service/RegisterUserService.java
@@ -0,0 +1,16 @@
+package com.bree.springproject.bursaryapplication.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RegisterUserService {
+
+}
--- src/main/java/com/bree/springproject/bursaryapplication/controller/RegisterUserController.java
+++ src/main/java/com/bree/springproject/bursaryapplication/controller/RegisterUserController.java
@@ -1,7 +1,8 @@
 package com.bree.springproject.bursaryapplication.controller;
 
-import com.bree.springproject.bursaryapplication.Entity.UserRegistrationTable;
+
 import com.bree.springproject.bursaryapplication.Models.UserRegistrationModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/home")
 
 public class RegisterUserController {
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody UserRegistrationModel userRegistrationModel){
+
 
 
         return null;
