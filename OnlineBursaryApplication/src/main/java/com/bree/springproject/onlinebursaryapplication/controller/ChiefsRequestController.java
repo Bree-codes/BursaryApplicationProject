a6f1e979d0f7c5c34d37df762e9a41711aabbe8c
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-13 12:08:21 +0300

    handling receiving of the forms values

--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/ChiefsRequestController.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/ChiefsRequestController.java
@@ -1,10 +1,12 @@
 package com.bree.springproject.onlinebursaryapplication.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v0/chief")
+@Slf4j
 public class ChiefsRequestController {
 
 
---
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/service/handleChiefLogicService.java
@@ -0,0 +1,17 @@
+package com.bree.springproject.onlinebursaryapplication.service;
+
+import com.bree.springproject.onlinebursaryapplication.models.ResponseModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class handleChiefLogicService {
+
+
+    public ResponseEntity<ResponseModel> receiveUserApplicationForm()
+    {
+
+    }
+}
