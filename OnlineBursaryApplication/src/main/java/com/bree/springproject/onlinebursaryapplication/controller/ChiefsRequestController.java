0f7f00ee510e55e84bf842292aca8323e4388259
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-13 11:43:26 +0300

    working on chief's requests

---
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/ChiefsRequestController.java
@@ -0,0 +1,12 @@
+package com.bree.springproject.onlinebursaryapplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v0/chief")
+public class ChiefsRequestController {
+
+
+
+}
