432c1602ea94ae9b0681139c72d3f0768c4e5c5e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-15 12:08:12 +0300

    working on view get forms

--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/ViewFormsController.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/ViewFormsController.java
@@ -1,6 +1,8 @@
 package com.bree.springproject.onlinebursaryapplication.controller;
 
+import com.bree.springproject.onlinebursaryapplication.service.ViewLogicService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +13,14 @@ import java.util.List;
 @Slf4j
 public class ViewFormsController {
 
+    @Autowired
+    ViewLogicService viewLogicService;
+
     @GetMapping("/view-forms/{viewerId}")
     public ResponseEntity<List<Long>> viewAvailableForms(
             @PathVariable String viewerId)
     {
-
-        return null;
+        log.info("Received A Request Form A viewer To Get Consented Froms");
+        return viewLogicService.getAvaibleForms(viewerId);
     }
 }
--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/service/ViewLogicService.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/service/ViewLogicService.java
@@ -2,9 +2,18 @@ package com.bree.springproject.onlinebursaryapplication.service;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class ViewLogicService {
+    public ResponseEntity<List<Long>> getAvaibleForms(String viewerId) {
+
+
+
+        return null;
+    }
 }
