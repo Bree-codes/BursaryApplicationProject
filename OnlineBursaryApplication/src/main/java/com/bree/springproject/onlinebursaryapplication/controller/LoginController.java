af4a763a95244a5b5f69180933309cc039c70912
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-17 14:06:07 +0300

    working on login

--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/LoginController.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.bree.springproject.onlinebursaryapplication.controller;
 
 import com.bree.springproject.onlinebursaryapplication.models.LoginModel;
+import com.bree.springproject.onlinebursaryapplication.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 public class LoginController {
 
+    @Autowired
+    private LoginService loginService;
+
     @GetMapping("/login")
     public ResponseEntity<LoginModel> login(@RequestParam String username,@RequestParam String password){
         log.info("Received login request");
-        return
+        return loginService.loginUser(username,password);
     }
 }
 
--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/service/LoginService.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/service/LoginService.java
@@ -1,4 +1,19 @@
 package com.bree.springproject.onlinebursaryapplication.service;
 
+import com.bree.springproject.onlinebursaryapplication.models.LoginModel;
+import com.bree.springproject.onlinebursaryapplication.repository.UserRegistrationRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class LoginService {
+
+    @Autowired
+    private UserRegistrationRepository userRegistrationRepository;
+    public ResponseEntity<LoginModel> loginUser(String username, String password) {
+        return null;
+    }
 }
