93a17ad762b4241fd2e85a83f4c63e28c8f47e52
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-17 14:01:20 +0300

    working on login

--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/LoginController.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/LoginController.java
@@ -1,14 +1,20 @@
 package com.bree.springproject.onlinebursaryapplication.controller;
 
+import com.bree.springproject.onlinebursaryapplication.models.LoginModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
 public class LoginController {
 
     @GetMapping("/login")
-    String login(){
-        return "login";
+    public ResponseEntity<LoginModel> login(@RequestParam String username,@RequestParam String password){
+        log.info("Received login request");
+        return
     }
 }
 
--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/models/LoginModel.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/models/LoginModel.java
@@ -1,4 +1,20 @@
 package com.bree.springproject.onlinebursaryapplication.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
 public class LoginModel {
+
+    private Long userId;
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private Boolean status;
+    private String role;
 }
