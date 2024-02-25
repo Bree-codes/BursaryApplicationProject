4154f7ac5fe9baf1f29da0a481ebed89a90f1f87
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-25 17:37:17 +0300

    login done

--- OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/LoginController.java
+++ OnlineBursaryApplication/src/main/java/com/bree/springproject/onlinebursaryapplication/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.bree.springproject.onlinebursaryapplication.controller;
 
+import com.bree.springproject.onlinebursaryapplication.models.AuthenticationResponseModel;
 import com.bree.springproject.onlinebursaryapplication.models.LoginModel;
 import com.bree.springproject.onlinebursaryapplication.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @Slf4j
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @GetMapping("/login")
+    public ResponseEntity<AuthenticationResponseModel> login(
+            @RequestBody LoginModel loginModel){
+        log.info("received a login request");
+       return ResponseEntity.ok(loginService.login(loginModel));
+    }
 
 
 }
