22b8ac4afa893272a7d9853891bb5241746cb364
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-29 17:55:28 +0300

    adding the styles

--- bursary-application-project/src/Components/InputComponent.jsx
+++ bursary-application-project/src/Components/InputComponent.jsx
@@ -4,13 +4,13 @@ import {InputGroup, Form} from "react-bootstrap";
 const InputComponent = () =>{
     return(
         <>
-            <InputGroup size="lg" className={"m-3 w-50 border-opacity-10"}>
-                <InputGroup.Text className="px-5 mx-4"
-                                 id="inputGroup-sizing-lg">User Name :
+            <InputGroup className="mb-3">
+                <InputGroup.Text id="inputGroup-sizing-default">
+                    Default
                 </InputGroup.Text>
                 <Form.Control
-                    aria-label="Large"
-                    aria-describedby="inputGroup-sizing-sm"
+                    aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default"
                 />
             </InputGroup>
         </>
--- bursary-application-project/src/main.jsx
+++ bursary-application-project/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
