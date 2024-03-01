c0f65611e91061304e0f2902a9edf1d8dfb695b1
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-01 14:54:36 +0300

    weeeh

--- bursary-application-project/src/Pages/RegistrationPage.jsx
+++ bursary-application-project/src/Pages/RegistrationPage.jsx
@@ -1,6 +1,4 @@
-// RegistrationPage.jsx
-
-import {Alert, Container} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import CreateAccountLayout from "../Layouts/CreateAccountLayout.jsx";
 import { useState } from "react";
 
@@ -10,29 +8,14 @@ const RegistrationPage = () => {
     const [userPhoneNumber, setUserPhoneNumber] = useState("");
     const [userPassword, setUserPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [alert, setAlert] = useState("");
 
-    /*This function will handle */
-    const handleRegister = () => {
+    const  handleRegister = () =>{
 
-        /*check if any of the functions are null*/
-        (userPassword === null || userName === null ||  userPhoneNumber === null)
-        && setAlert("The username, phone number and password can not be empty")
+    }
 
-        try {
-            parseInt(userPassword)
-        }catch (error){
-            setAlert("phone number must be contain numbers only")
-        }
-
-        /*here we will handle the password pattern*/
-
-
-    };
 
     return (
         <Container>
-            <div>{alert}</div>
             <CreateAccountLayout
                 userName={userName}
                 setUserName={setUserName}
