9ca142904a8c82dc46d363da408f9cbc5ec81cf2
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-04 20:23:59 +0300

    handled the admin page design

--- bursary-application-project/src/Components/FormFunctions/<PageTemplateNavigationBar.jsx
+++ bursary-application-project/src/Components/FormFunctions/<PageTemplateNavigationBar.jsx
@@ -1,5 +1,6 @@
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import OffCanvas from "../OffCanvas.jsx";
+import {Link} from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const PageTemplateNavigationBar = ({action, handleApply, viewQualified}) =>{
@@ -7,13 +8,22 @@ const PageTemplateNavigationBar = ({action, handleApply, viewQualified}) =>{
 
     return(
         <>
-            <Navbar bg="primary" data-bs-theme="dark" className={"align-content-end"} >
+            <Navbar style={{
+                backgroundColor: 'rgba(15,235,202,0.5)',
+            }} data-bs-theme="dark" className={"align-content-end"} >
                 <Container className={"align-content-end"} >
-                    <Navbar.Brand className={"align-content-end"}><OffCanvas  placement={"start"}
-                                              name={(localStorage.getItem('role')).toUpperCase()+"'S  Dashboard "}
+                    <Navbar.Brand className={"align-content-end"}>
+                                    <OffCanvas
+                                        placement={"start"}
+                                        name={(localStorage.getItem('role')).
+                                        toUpperCase()+"'S  Dashboard "}
                     /></Navbar.Brand>
                     <Nav className="me-auto">
-                        <Button className={"fw-bolder px-5 mx-5 "} onClick={handleApply}>{action}</Button>
+                        <Button className={"fw-bolder p-2 m-2 "} onClick={handleApply} style={{
+                            backgroundColor: 'rgba(0,0,0,0.3)',
+                            border:'none',
+                        }}>{action}</Button>
+                        {viewQualified && <>{viewQualified}</>}
                     </Nav>
                 </Container>
             </Navbar>
--- bursary-application-project/src/Layouts/AdminsPageLayout.jsx
+++ bursary-application-project/src/Layouts/AdminsPageLayout.jsx
@@ -1,32 +1,43 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import PageTemplateNavigationBar from "../Components/FormFunctions/<PageTemplateNavigationBar.jsx";
 import {useNavigate} from "react-router";
 import {Col, Stack} from "react-bootstrap";
 import AddUserComponent from "../Components/AddUserComponent.jsx";
 import UserCreationNotes from "../Components/UserCreationNotes.jsx";
+import {Link} from "react-router-dom";
 
 const AdminsPageLayout = () => {
     const [pageData, setPageData] = useState(null);
     const navigate = useNavigate();
     const [userCreationRules, setUserCreationRules] = useState(null);
+    const [qualifiedLink, setQualifiedLink] = useState(null);
+
+    useEffect(() => {
+        setQualifiedLink(<Link
+            className={"fw-bolder m-2 p-2 rounded-3"}
+            style={{color:'white', backgroundColor: 'rgba(0,0,0,0.3)',}}
+            to={"/admin/qualified"}>Qualified Students</Link>)
+        }, []);
+
+
 
     const handleAddUser = () => {
         console.log("Navigating to add user...")
-        navigate("add-user")
-
         setPageData(<AddUserComponent />)
         setUserCreationRules(<UserCreationNotes />)
     }
 
     return (
         <>
-            <PageTemplateNavigationBar handleApply={handleAddUser} action={"Add User"} />
+            <PageTemplateNavigationBar handleApply={handleAddUser}
+                                       action={"Add User"}
+                                       viewQualified={qualifiedLink}/>
             <Stack direction={"horizontal"}>
                 {pageData && <Col>{ pageData }</Col>}
-                {userCreationRules && <Col className={"shadow z-2 "} md={"auto"} style={{
+
+                {    userCreationRules && <Col className={"shadow z-2 "} md={"auto"} style={{
                     background:"wheat",
-                    padding:'10px',
-                }}>{userCreationRules}</Col>}
+                    padding:'10px'}}>{userCreationRules}</Col>}
             </Stack>
         </>
     );
