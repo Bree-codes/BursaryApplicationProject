aaa2305facf536d674a8c5881b94a881b88db36a
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-04 10:36:50 +0300

    creating the user creation form

---
+++ bursary-application-project/src/Components/AddUserComponent.jsx
@@ -0,0 +1,25 @@
+import {Col, Row} from "react-bootstrap";
+import InputComponent from "./InputComponent.jsx";
+import {useState} from "react";
+
+const AddUserComponent = () =>{
+    const [username, setUsername] = useState("");
+
+
+
+    return(
+        <Row>
+            <Col>
+                <InputComponent filedName={"Username"}
+                                type={"text"}
+                                placeHolder={"eg. Chief Kimathi Location"}
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username}
+                                />
+
+            </Col>
+        </Row>
+    )
+}
+
+export default AddUserComponent;
