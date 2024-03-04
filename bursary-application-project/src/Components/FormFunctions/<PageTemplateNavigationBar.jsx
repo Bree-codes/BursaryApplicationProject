5b6b5e1268d1fcf4f82b7e98afbfd476016ffbff
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-04 10:12:13 +0300

    working on cheif's functionalities

--- bursary-application-project/src/Components/FormFunctions/<PageTemplateNavigationBar.jsx
+++ bursary-application-project/src/Components/FormFunctions/<PageTemplateNavigationBar.jsx
@@ -2,14 +2,14 @@ import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import OffCanvas from "../OffCanvas.jsx";
 
 // eslint-disable-next-line react/prop-types
-const PageTemplateNavigationBar = ({action, handleApply}) =>{
+const PageTemplateNavigationBar = ({action, handleApply, addUser, viewQualified}) =>{
 
 
     return(
         <>
             <Navbar bg="primary" data-bs-theme="dark" className={"align-content-end"} >
-                <Container >
-                    <Navbar.Brand ><OffCanvas  placement={"start"}
+                <Container className={"align-content-end"} >
+                    <Navbar.Brand className={"align-content-end"}><OffCanvas  placement={"start"}
                                               name={(localStorage.getItem('role')).toUpperCase()+"'S  Dashboard "}
                     /></Navbar.Brand>
                     <Nav className="me-auto">
