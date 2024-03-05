fa4a196ecf85ff4c5271888594d1d358c9d90b12
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-05 10:45:18 +0300

    desgned the create form button

--- bursary-application-project/src/Components/MenuButton.jsx
+++ bursary-application-project/src/Components/MenuButton.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 
-const MenuButton = () => {
+// eslint-disable-next-line react/prop-types
+const MenuButton = ({title,onclick}) => {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div style={{width: "99%", margin: "10px auto"}}>
+        <div style={{width: "99%", margin: "2px auto"}}>
             <Button
-                className={"p-2 m-2 mt-3"}
+                className={"p-2"}
                 style={{
                     width: "100%",
                     background: isHovered ? "lime" : "transparent",
@@ -16,8 +17,9 @@ const MenuButton = () => {
                 }}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
+                onClick={onclick}
             >
-                Create Form
+                {title}
             </Button>
         </div>
     );
--- bursary-application-project/src/Views/ViewerView/ViewerIndexPage.jsx
+++ bursary-application-project/src/Views/ViewerView/ViewerIndexPage.jsx
@@ -1,23 +1,34 @@
 import {Col, Container, Row} from "react-bootstrap";
 import PageTemplateNavigationBar from "../../Components/FormFunctions/<PageTemplateNavigationBar.jsx";
 import MenuButton from "../../Components/MenuButton.jsx";
+import {useState} from "react";
 
 const ViewerIndexPage = () => {
+    const [mainContain, setMainConsent] = useState(null);
+
+
+
+
+
     return (
         <>
             <PageTemplateNavigationBar />
             <Container fluid>
                 <Row>
                     <Col xs={12} md={3} className={"bg-dark"}
-                        style={{
-                            backgroundColor: "#f8f9fa",
-                            minHeight: "100vh",
-                            position: "fixed",
-                            left: 0,
-                            top: "4rem",
-                            zIndex: 1,}}>
-
-                        <MenuButton />
+                         style={{
+                             backgroundColor: "#f8f9fa",
+                             minHeight: "100vh",
+                             position: "fixed",
+                             left: 0,
+                             top: "4rem",
+                             zIndex: 1,
+                         }}>
+                        <hr style={{color: 'white'}}/>
+                        <MenuButton title={"Create Form"} onclick={() => {
+                            setMainConsent();
+                        }}/>
+                        <hr style={{color: 'white'}}/>
                     </Col>
                     <Col
                         xs={12}
@@ -25,7 +36,6 @@ const ViewerIndexPage = () => {
                         style={{
                             paddingTop: "4rem",
                             zIndex: 0,}}>
-                        Main Content
 
                     </Col>
                 </Row>
